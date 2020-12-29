18b9040d0aa05b81a2b69da7782c07f44509b24d
Mark <58929072+623Mark@users.noreply.github.com>
2020-12-29 21:26:25 +0700

    Add files via upload

---
+++ 623/src/buathan/chanon/lab2/CamelCaseNaming.java
@@ -0,0 +1,40 @@
+package buathan.chanon.lab2;
+
+// The CamelCaseNaming program which reflect programmer coding style that's (_^) which means the second letter must be
+// Uppercase for easily readable which other can understand in instant.
+
+/*
+ * Author: Chanon Buathan
+ * ID: 623040421-0
+ * Sec: 1
+ * Date: December 29, 2020
+ */
+
+public class CamelCaseNaming {
+    public static void main(String[] args) {
+        if (args.length != 2)  {
+            System.err.println("CamelCaseNaming : <First word> <Second word>");
+        } else {
+            String convertOne = args[0];
+            String convertTwo = args[1];
+
+            // Substring in Java, I also used [0,1] ([1] letter from the first string becomes UpperCase)
+                                            // ^ start from 1 and end in next letter
+            // and used (1) for skip counting the first one letter in contrast start (skip-1) and going on to the rest.
+            String firstLetter = convertOne.substring(0,1);
+            String remainLetter = convertOne.substring(1);
+            String first = firstLetter.toUpperCase()+remainLetter.toLowerCase();
+
+            // Did the same things to args[1]
+            String secondLetter = convertTwo.substring(0,1);
+            String remainScLetter = convertTwo.substring(1);
+            String second = secondLetter.toUpperCase()+remainScLetter.toLowerCase();
+
+            // concatenate which used + .
+            System.out.println("The first word is "+args[0]);
+            System.out.println("The second word is "+args[1]);
+            System.out.println("The concatenate with camel case is "+ first + second);
+        }
+
+    }
+}
---
+++ 623/src/buathan/chanon/lab2/CircleCalculator.java
@@ -0,0 +1,44 @@
+package buathan.chanon.lab2;
+import java.lang.Math;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+// The CircleCalculator showing the calculate which is from the area of circle, and perimeter.
+
+/*
+ * Author: Chanon Buathan
+ * ID: 623040421-0
+ * Sec: 1
+ * Date: December 29, 2020
+ */
+
+public class CircleCalculator {
+    public static void main(String[] args) {
+        if (args.length != 1){
+            System.err.println("CircleCalculator <radius of a circle>");
+        } else {
+            String inputRadius = args[0];
+            float radius = Float.parseFloat(inputRadius);
+            float circleArea = (float) (radius*radius*Math.PI);
+            float perimeter = (float) (2*radius*Math.PI);
+            // ("%.2f", How to print a float with 2 decimal places in Java?
+
+            /*
+            float twoDecimal
+            System.out.println("An area of a circle with radius of " + radius + " is " + "%1.2f", circleArea);
+            System.out.println("A circumference is ");
+            */
+
+            // Big decimal method with setScale used for print a float with 2(from setScale) decimal.
+            BigDecimal bdArea = new BigDecimal(circleArea).setScale(2, RoundingMode.HALF_UP);
+            double twoDecimalcircle = bdArea.doubleValue();
+
+            BigDecimal bdPerimeter = new BigDecimal(perimeter).setScale(2, RoundingMode.HALF_UP);
+            double twoDecimalperimeter = bdPerimeter.doubleValue();
+
+            System.out.println("An area of a circle with radius of " + radius + " is " + twoDecimalcircle);
+            System.out.println("A circumference is "+twoDecimalperimeter);
+        }
+    }
+}
