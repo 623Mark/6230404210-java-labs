a3934b40033bf19576051d0768131e5263649191
Mark <58929072+623Mark@users.noreply.github.com>
2021-01-04 14:24:04 +0700

    Update PrimitiveDataType.java

--- 623/src/buathan/chanon/lab2/PrimitiveDataType.java
+++ 623/src/buathan/chanon/lab2/PrimitiveDataType.java
@@ -14,11 +14,12 @@ public class PrimitiveDataType {
         byte myByte = 120;                          // Stores whole numbers from -128 to 127
         short myShort = 32000;                      // Stores whole numbers from -32,768 to 32,767
         int myNum = 2000000000;                     // Stores whole numbers from -2,147,483,648 to 2,147,483,647
-        long myLong =9000000000000000l;             // Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        long myLong = 9000000000000000l;             // Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
         float myFloatNum = 1.123456f;               // Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
         double myDouble = 1.12345678910;            // Stores fractional numbers. Sufficient for storing 15 decimal digits
         char myLetter = 'a';                        // Stores a single character/letter or ASCII values
         boolean myBool = true;                      // Stores true or false values
+        // ref for all detail above from " https://www.w3schools.com/java/java_data_types.asp "
         // String myText = "Hello";
         // Represent the eight primitive data types in Java.
         System.out.println(myBool);
