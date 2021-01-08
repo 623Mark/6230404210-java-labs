7e4004109def7a72fc71d5d0155374bb0025ba24
623Mark <bu_chanon@kkumail.com>
2021-01-08 21:51:31 +0700

    git commit -m "wai ma kae NoSuchElementException"

--- 623/src/buathan/chanon/lab3/GuessNumberGame.java
+++ 623/src/buathan/chanon/lab3/GuessNumberGame.java
@@ -4,29 +4,25 @@ import java.util.Scanner;
 
 public class GuessNumberGame {
     public static void main(String[] args) {
-        int convertNumber;
-        Scanner inputNum = new Scanner(System.in);
-
         int minNum = 1;
         int maxNum = 10;
-        int randomFormula = minNum + (int)( Math.random() * ( ( maxNum - minNum ) + 1 ) );
+        int randomNum = minNum + (int)( Math.random() * ( ( maxNum - minNum ) + 1 ) );
+        System.out.print("Please enter a guess (1-10):");
 
-// Please enter a guess (1-10):
-// Please type a lower number! Number of remaining tries:
-
-        int amount = 3;
-        while (amount != 0);
-        System.out.println("Please enter a guess (1-10):");
-        convertNumber = inputNum.nextInt();
-            /*
-            if ( convertNumber == randomFormula ) {
-                System.out.println("Congratulations! That's correct");
-            } else {
-                System.out.println("Please type a lower number! Number of remaining tries:");
-            */
-        switch (randomFormula){
-
-
+        int remainTry = 3;
+        while (remainTry != 0){
+            int inputNumGuessing;
+            Scanner inputNum = new Scanner(System.in);
+            inputNumGuessing = inputNum.nextInt();
+                if ( inputNumGuessing == randomNum ) {
+                    System.out.println("Congratulations! That's correct");
+                } else if ( inputNumGuessing > randomNum) {
+                    System.out.print("Please type a lower number! Number of remaining tries:" + remainTry);
+                } else {
+                    System.out.print("Please type a higher number! Number of remaining tries:" + remainTry);
+        }
+            remainTry--;
+    inputNum.close();
         }
     }
 }
