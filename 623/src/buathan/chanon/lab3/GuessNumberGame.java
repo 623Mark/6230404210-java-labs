7f7b1ffc261f8fea27e88bd5ed5904b634c3af1f
623Mark <bu_chanon@kkumail.com>
2021-01-09 19:36:15 +0700

    Test for friend

--- 623/src/buathan/chanon/lab3/GuessNumberGame.java
+++ 623/src/buathan/chanon/lab3/GuessNumberGame.java
@@ -8,8 +8,9 @@ public class GuessNumberGame {
         int maxNum = 10;
         int randomNum = minNum + (int)( Math.random() * ( ( maxNum - minNum ) + 1 ) );
         System.out.print("Please enter a guess (1-10):");
+        //String welcomeMessage = "Please enter a guess (1-10):";
 
-        int remainTry = 3;
+        int remainTry = 2;
         while (remainTry != 0){
             int inputNumGuessing;
             Scanner inputNum = new Scanner(System.in);
@@ -22,6 +23,7 @@ public class GuessNumberGame {
                     System.out.print("Please type a higher number! Number of remaining tries:" + remainTry);
         }
             remainTry--;
+                ++remainTry;
     inputNum.close();
         }
     }
