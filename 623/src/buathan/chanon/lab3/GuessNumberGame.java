1767fb83955d8d54edea24d223d06715a16bb610
623Mark <bu_chanon@kkumail.com>
2021-01-09 20:41:54 +0700

    Completed on lab 3 problem 1; Unnecessary string in /else if/ not used I'll fix it later.

--- 623/src/buathan/chanon/lab3/GuessNumberGame.java
+++ 623/src/buathan/chanon/lab3/GuessNumberGame.java
@@ -7,24 +7,31 @@ public class GuessNumberGame {
         int minNum = 1;
         int maxNum = 10;
         int randomNum = minNum + (int)( Math.random() * ( ( maxNum - minNum ) + 1 ) );
-        System.out.print("Please enter a guess (1-10):");
-        //String welcomeMessage = "Please enter a guess (1-10):";
+        int remainTry = 3;
+        Scanner inputNum = new Scanner(System.in);
 
-        int remainTry = 2;
-        while (remainTry != 0){
+        while (remainTry > 0){
+            remainTry--;
             int inputNumGuessing;
-            Scanner inputNum = new Scanner(System.in);
+            String welcomeGuess = "Please enter a guess (1-10):";
+            System.out.print(welcomeGuess);
             inputNumGuessing = inputNum.nextInt();
+            inputNum.hasNextLine();
+
                 if ( inputNumGuessing == randomNum ) {
                     System.out.println("Congratulations! That's correct");
+                    break;
                 } else if ( inputNumGuessing > randomNum) {
                     System.out.print("Please type a lower number! Number of remaining tries:" + remainTry);
-                } else {
+                    System.out.println("");
+                    continue;
+                } else if ( inputNumGuessing < randomNum) {
                     System.out.print("Please type a higher number! Number of remaining tries:" + remainTry);
-        }
-            remainTry--;
-                ++remainTry;
-    inputNum.close();
+                    System.out.println("");
+                    continue;
+                } else if ( remainTry == 0) {
+                    break;
+            }
         }
     }
 }
