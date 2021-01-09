07025e24208dde1905510ab824a815d0d60fcbd9
623Mark <bu_chanon@kkumail.com>
2021-01-10 01:26:26 +0700

    Completed GuessNumberGame

--- 623/src/buathan/chanon/lab3/GuessNumberGame.java
+++ 623/src/buathan/chanon/lab3/GuessNumberGame.java
@@ -2,35 +2,51 @@ package buathan.chanon.lab3;
 
 import java.util.Scanner;
 
+// GuessNumberGame, the game that's made by a simple rule.
+// User can enter some number between (1-10) and the program could calculate random number via using random function.
+// If it right user will give appreciate words with "Congratulations! That's correct" on the edge of the screen,
+// if not a little change passage will come out as that could be. Also showing the remaining guess.
+
+/*
+ * Author: Chanon Buathan
+ * ID: 623040421-0
+ * Sec: 1
+ * Date: January 10, 2020
+ */
+
 public class GuessNumberGame {
     public static void main(String[] args) {
         int minNum = 1;
         int maxNum = 10;
         int randomNum = minNum + (int)( Math.random() * ( ( maxNum - minNum ) + 1 ) );
-        int remainTry = 3;
+        int remainTry = 3;  // counter loop
         Scanner inputNum = new Scanner(System.in);
 
-        while (remainTry > 0){
+        // Scanner in loop
+        while ( remainTry > 0 ){
             remainTry--;
             int inputNumGuessing;
-            String welcomeGuess = "Please enter a guess (1-10):";
-            System.out.print(welcomeGuess);
+            String welcomeGuessText = "Please enter a guess (1-10):";
+            System.out.print(welcomeGuessText);
             inputNumGuessing = inputNum.nextInt();
-            inputNum.hasNextLine();
+            inputNum.hasNextLine(); // for avoid error as such NoSuchElementException by using multiple buffered in input
+            // also using .hasNextLine to received String and convert to int via .nextInt()
+            // ref: https://stackoverflow.com/questions/14720662/java-loop-user-input-from-scanner
+            // ref: https://stackoverflow.com/questions/15398703/exception-in-thread-main-java-util-nosuchelementexception
 
                 if ( inputNumGuessing == randomNum ) {
                     System.out.println("Congratulations! That's correct");
                     break;
-                } else if ( inputNumGuessing > randomNum) {
+                } else if ( inputNumGuessing > randomNum ) {
                     System.out.print("Please type a lower number! Number of remaining tries:" + remainTry);
                     System.out.println("");
                     continue;
-                } else if ( inputNumGuessing < randomNum) {
+                } else if ( inputNumGuessing < randomNum ) {
                     System.out.print("Please type a higher number! Number of remaining tries:" + remainTry);
                     System.out.println("");
                     continue;
-                } else if ( remainTry == 0) {
-                    break;
+                    // Therefore, the last "else if" can amend to "} else {" for the outsider case 1(==) 2(>)
+                    // but considering that it's good to readable for others.
             }
         }
     }
