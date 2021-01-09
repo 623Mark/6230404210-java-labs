c4412fe055cc913bc5a81b8d33e20fd17a7c2371
623Mark <bu_chanon@kkumail.com>
2021-01-10 01:05:12 +0700

    tiny change in style

--- 623/src/buathan/chanon/lab2/CamelCaseNaming.java
+++ 623/src/buathan/chanon/lab2/CamelCaseNaming.java
@@ -12,29 +12,28 @@ package buathan.chanon.lab2;
 
 public class CamelCaseNaming {
     public static void main(String[] args) {
-        if (args.length != 2)  {
-            System.err.println("CamelCaseNaming : <First word> <Second word>");
+        if ( args.length != 2 ) {
+            System.err.println( "CamelCaseNaming : <First word> <Second word>" );
         } else {
-            String convertOne = args[0];
-            String convertTwo = args[1];
+            String firstArgs = args[0];
+            String secondArgs = args[1];
 
             // Substring in Java, I also used [0,1] ([1] letter from the first string becomes UpperCase)
                                             // ^ start from 1 and end in next letter
             // and used (1) for skip counting the first one letter in contrast start (skip-1) and going on to the rest.
-            String firstLetter = convertOne.substring(0,1);
-            String remainLetter = convertOne.substring(1);
-            String first = firstLetter.toUpperCase() + remainLetter.toLowerCase();
+            String firstLetter = firstArgs.substring(0,1);
+            String remainLetter = firstArgs.substring(1);
+            String firstWord = firstLetter.toUpperCase() + remainLetter.toLowerCase();
 
-            // Did the same things to args[1]
-            String secondLetter = convertTwo.substring(0,1);
-            String remainScLetter = convertTwo.substring(1);
-            String second = secondLetter.toUpperCase() + remainScLetter.toLowerCase();
+            // Same things goes for args[1]
+            String secondLetter = secondArgs.substring(0,1);
+            String remainScLetter = secondArgs.substring(1);
+            String secondWord = secondLetter.toUpperCase() + remainScLetter.toLowerCase();
 
             // concatenate which used + .
-            System.out.println("The first word is " + args[0]);
-            System.out.println("The second word is " + args[1]);
-            System.out.println("The concatenate with camel case is " + first + second);
+            System.out.println( "The first word is " + firstWord );
+            System.out.println( "The second word is " + secondWord );
+            System.out.println( "The concatenate with camel case is " + firstWord + secondWord );
         }
-
     }
 }
