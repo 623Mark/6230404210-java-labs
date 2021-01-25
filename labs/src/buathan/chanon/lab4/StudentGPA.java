7dbd1dfdf25903f3f2a86a1d3d8629fac34d3494
623Mark <bu_chanon@kkumail.com>
2021-01-25 22:59:15 +0700

    lab4 pb4 what

--- labs/src/buathan/chanon/lab4/StudentGPA.java
+++ labs/src/buathan/chanon/lab4/StudentGPA.java
@@ -19,60 +19,77 @@ public class StudentGPA {
     static String[] grades = new String[MAX_NUMBER_OF_COURSE]; // A, B+, B, C+, C, D+, D, F
 
     static int numberOfCourse = 0;
+    //static int counterLoop = 1;
 
     static Scanner inputInfo = new Scanner(System.in);
 
     public static void main(String[] args) {
-        questionUserToDo();
+        //questionUserToDo();
+        first();
         inputGrades();
         showGPA();
     }
 
-    public static void questionUserToDo() {
+    public static void first() {
         System.out.println("Enter grades of student (year term name_course credit grade).");
         System.out.println("Finish when enter year with 0");
         return;
     }
 
-    public static void questionFunctionToDo() {
-
-    }
-
     public static void inputGrades() {
         int yearInfo = inputInfo.nextInt();
-        while (true) {
-            if (yearInfo <= 0) {
-                //questionFunctionToDo();
-                System.out.println("Type in 'o' to see all courses, 'a' for accumulated GPA, 't' for GPA for specific term or 'q' to exit");
-                String inputCommand = inputInfo.next();
-                if (inputCommand.equals("o")) {
-                    inputGrades();
+    /*
+        while (yearInfo >= 0) {
+            continue;
+        }
+     */
+        if (yearInfo == 0) {
+            System.out.println("Type in 'o' to see all courses, 'a' for accumulated GPA, 't' for GPA for specific term or 'q' to exit");
+            String inputCommand = inputInfo.next();
 
-                } else if (inputCommand.equals("a")) {
-                    inputGrades();
+            if (inputCommand.equals("o")) {
+                inputGrades();
 
-                } else if (inputCommand.equals("t")) {
-                    inputGrades();
+            } else if (inputCommand.equals("a")) {
+                inputGrades();
 
-                } else if (inputCommand.equals("q")) {
-                    System.exit(0);
-                }
-                // else for (yearInfo > 0) looping scanner input
+            } else if (inputCommand.equals("t")) {
+                inputGrades();
+
+            } else if (inputCommand.equals("q")) {
+                System.exit(0);
+
             } else {
-                int termInfo = inputInfo.nextInt();
-                String courseName = inputInfo.next();
-                int courseCredit = inputInfo.nextInt();
-                String grade = inputInfo.next();
 
+
+                //termInfo = new int[MAX_NUMBER_OF_COURSE];
+                for (numberOfCourse = 0; numberOfCourse < MAX_NUMBER_OF_COURSE; numberOfCourse++) {
+
+                    //inputGrades.yearInfo[numberOfCourse] = inputInfo.nextInt();
+                    term[numberOfCourse] = inputInfo.nextInt();
+                    courseName[numberOfCourse] = inputInfo.next();
+                    courseCredit[numberOfCourse] = inputInfo.nextInt();
+                    grades[numberOfCourse] = inputInfo.next();
+
+                            /*
+                    System.out.println(year[numberOfCourse] + " ");
+                    System.out.println(term[numberOfCourse] + " ");
+                    System.out.println(courseName[numberOfCourse] + " ");
+                    System.out.println(courseCredit[numberOfCourse] + " ");
+                    System.out.println(grades[numberOfCourse] + " ");
+                    */
+                }
             }
         }
     }
 
-        //-----------------------------------------------------------------//
-        //  Methods to input Term, Grades and print the accumulated GPA    //
-        //-----------------------------------------------------------------//
-        public static void showGPA () {
-            System.out.println("Accumulated GPA is " + "3.0");
+        public static void loopArray() {
+
         }
 
+
+        public static void showGPA() {
+            System.out.println("Accumulated GPA is " + "3.0");
+        }
     }
+
