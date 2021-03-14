636d2586dfccc41f24e9ffa50768daf36a8256f7
623Mark <bu_chanon@kkumail.com>
2021-03-14 23:49:01 +0700

    done (2/3); lab7

--- labs/src/buathan/chanon/lab7/TestInterfaceAsType.java
+++ labs/src/buathan/chanon/lab7/TestInterfaceAsType.java
@@ -1,6 +1,6 @@
 
 /**
- *  TestInterfaceAsType, the program which
+ *  TestInterfaceAsType, the program which used interface to compare roll dice.
  *
  */
 
@@ -9,37 +9,18 @@
  * ID: 623040421-0
  * Sec: 1
  * Date: March 14, 2021
- *
- *
  */
 
 package buathan.chanon.lab7;
 import buathan.chanon.lab6.*;
 import java.util.ArrayList;
 
-interface UseBoard
+public class TestInterfaceAsType
 {
-    public void setUpBoard();
-}
-
-interface HasRule
-{
-    public String gameRules();
-}
-
-interface UseDice
-{
-    public int rollDice();
-}
-
-
-
-
-public class TestInterfaceAsType {
-    public static int compareRollDice(DiceGame g1, DiceGame g2) {
-
-
-        return 0;
+    public static int compareRollDice(DiceGame g1, DiceGame g2)
+    {
+        int diceRollCompare = g1.getDiceRoll() - g2.getDiceRoll();
+        return diceRollCompare;
     }
 
     public static void main(String[] args) {
@@ -49,6 +30,23 @@ public class TestInterfaceAsType {
         games.add(new HighLowGame(4, "h"));
         games.add(new MonopolyGame());
         games.add(new MonopolyGame(3));
+
+        int[] rollDiceArray = new int[games.size()];
+        for (int i = 0; i < games.size(); i++)
+        {
+            rollDiceArray[i] = games.get(i).rollDice();
+        }
+
+        /*for (int i = 0; i < rollDiceArray.length - 1; i++)
+        {
+            if ( i < games.size())
+            {
+                if ( rollDiceArray[i] < rollDiceArray[i - 1 ])
+                {
+                    // System.out.println()
+                }
+            }
+        }*/
         System.out.println("Game(0):6 has dice roll less than Game(1):13");
         System.out.println("Game(0): rollDice method return s 9 ");
         System.out.println("Game(1):13 has dice roll less than Game(2):13");
